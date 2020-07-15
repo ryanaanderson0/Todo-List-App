5c41bac3ef5deb45a5394c75e074ca881fa9acdd
Ryan Anderson <ryanaanderson0@gmail.com>
2020-07-14 19:59:40 -0700

    added components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function TodoItem(props) {
+    return(
+        <div>
+            <input type="checkbox" checked={props.item.completed}/>
+            <p>{props.item.text}</p>
+        </div>
+    )
+}
+
+
+
+
+export default TodoItem
